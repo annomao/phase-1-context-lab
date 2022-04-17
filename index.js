16eeb72cfbc0fa1a690fc572e67a5d2b8397af56
annomao <omaoannkerubo@gmail.com>
2022-04-17 13:17:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 /* Your Code Here */
+let createEmployeeRecord = function (record){
+  return {
+    firstName :record[0],
+    familyName:record[1],
+    title:record[2],
+    payPerHour:record[3],
+    timeInEvents:[],
+    timeOutEvents:[]
+  }
+}
+let createEmployeeRecords = function(records){
+  return records.map(record =>{
+    return createEmployeeRecord(record)
+  })
+}
+let createTimeInEvent = function(time){
+  const [date,hour] = time.split(" ")
+  const timeInObject = {
+    type : "TimeIn",
+    hour : Number(hour),
+    date: date
+  }
+  this.timeInEvents.push(timeInObject)
+  return this
+}
+let createTimeOutEvent = function(time){
+  const [date,hour] = time.split(" ")
+  const timeOutObject = {
+    type : "TimeOut",
+    hour : Number(hour),
+    date: date
+  }
+  this.timeOutEvents.push(timeOutObject)
+  return this
+}
+let hoursWorkedOnDate = function(time){
 
+  let hourInObj = this.timeInEvents.find(obj =>{
+    return obj.date===time
+  })
+  let hourOutObj = this.timeOutEvents.find(obj =>{
+    return obj.date===time
+  })
+  return (hourOutObj.hour - hourInObj.hour)/100
+}
+let wagesEarnedOnDate = function(time){
+  const hoursWorked = hoursWorkedOnDate.call(this,time)
+  const rate = this.payPerHour
+  return hoursWorked * rate
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +70,18 @@ const allWagesFor = function () {
     return payable
 }
 
+let findEmployeeByFirstName = function (srcArray,firstname){
+  return srcArray.find(record =>{
+    return record.firstName === firstname
+  })
+}
+let calculatePayroll = function(employeeArray){
+  let employeeWages = employeeArray.map(record =>{
+    return allWagesFor.call(record)
+  })
+  
+  const totalEmployeeWages = employeeWages.reduce((previousValue,currentValue)=>{
+    return previousValue + currentValue
+  })
+  return totalEmployeeWages
+}
